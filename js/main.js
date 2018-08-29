07ec1eab5ff085375346fc7851a0409ef5a09c4b
Amera Abdallah <mera.a.abd@gmail.com>
2018-08-30 01:03:13 +0200

    fixing some issues

--- js/main.js
+++ js/main.js
@@ -14,6 +14,17 @@ if ('serviceWorker' in navigator) {
     });
 }
 
+// if ('serviceWorker' in navigator) {
+//   navigator.serviceWorker.register('/sw-test/sw.js', {scope: '/sw-test/'})
+//   .then(function(reg) {
+//     // registration worked
+//     console.log('Registration succeeded. Scope is ' + reg.scope);
+//   }).catch(function(error) {
+//     // registration failed
+//     console.log('Registration failed with ' + error);
+//   });
+// }
+
 /**
  * Fetch neighborhoods and cuisines as soon as the page is loaded.
  */
